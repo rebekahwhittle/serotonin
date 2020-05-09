b612a13c8c724f0da6bbadf3483a55d58ac737b3
Rebekah Whittle <whittlebekah@gmail.com>
2020-05-09 08:05:23 -0500

    ✨ serotonin pls

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          spare serotonin ma'am 🥺
+        </p>
+        <a
+          className="App-link"
+          href="https://openpuppies.com/#VDODyNP"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          dear god pls
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
