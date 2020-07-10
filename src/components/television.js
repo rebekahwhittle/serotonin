f3cdfc2572617b54e6430805860b77ce3a7db6a8
Rebekah Whittle <whittlebekah@gmail.com>
2020-07-10 09:44:12 -0500

    📺 atenna added

--- src/components/television.js
+++ src/components/television.js
@@ -6,7 +6,11 @@ import "../styles/main.scss";
 function television() {
   return (
     <div className="App">
-        <div className="flex column television-outline">
+        <div className="flex row atenna">
+            <div className="television-atenna left"></div>
+            <div className="television-atenna right"></div>
+        </div>
+        <div className="flex row television-outline">
             <div className="televsion-screen"></div>
             <div className="television-controls"></div>
 
