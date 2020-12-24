296070caaca7d6bdf0d83fbc75484907ffd413de
Rebekah Whittle <whittlebekah@gmail.com>
2020-12-24 09:52:48 -0600

    👩🏼‍💻  simplicity leads to complications

--- src/components/giphy.js
+++ src/components/giphy.js
@@ -5,29 +5,44 @@ import { Gif, Grid, Carousel } from "@giphy/react-components";
 import { useAsync } from "react-async-hook";
 import "../styles/main.scss";
 
+
 // set key for fetch
 const gf = new GiphyFetch("sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh");
 
+
 const carousel = async () => {
+  const getRandomOffset = (max) => {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+  let offsetN = getRandomOffset(500);
+  console.log(offsetN);
+
+
   let strings = ["cute animals", "kittens", "puppies", "bts", "taylor swift"]; 
   let inputs = strings[Math.floor(Math.random() * strings.length)];
 
-  try {
-    const gifs = await gf.search(inputs, { limit: 1 });
-    console.log(`carousel`, gifs);
-  } catch (error) {
-    console.error(`carousel`, error);
+  const gifs = await gf.search(inputs, { offset: offsetN });
+  let aGif = gifs.data[0].url;
+  console.log(`carousel`, aGif);
+
+  function CarouselDemo() {
+    const fetchGifs = (offset) =>
+      gf.search("dogs", { offset, limit: 10 });
+    return <Carousel fetchGifs={fetchGifs} gifHeight={200} gutter={6} />;
   }
 };
-
-// carousel();
 
 function giphy() {
   return (
     <div className="App">
       <button onClick={carousel}>click me!</button>
+      <div className="gify">
+      </div>
     </div>
   );
 }
+
+
+
 
 export default giphy;
