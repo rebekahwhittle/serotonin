2a081ec6a52696672f81c24037de4a0a52665d96
Rebekah Whittle <whittlebekah@gmail.com>
2020-12-25 17:43:39 -0600

    m 🎄

--- src/components/giphy.js
+++ src/components/giphy.js
@@ -9,30 +9,25 @@ import "../styles/main.scss";
 // set key for fetch
 const gf = new GiphyFetch("sXpGFDGZs0Dv1mmNFvYaGUvYwKX0PWIh");
 
-let thisGif;
-
-const randomGif = async () => {
-  var aGif;
-
-  const getRandomOffset = (max) => {
-    return Math.floor(Math.random() * Math.floor(max));
-  }
-  let offsetN = getRandomOffset(500);
-
-  let strings = ["cute animals", "kittens", "puppies", "bts", "taylor swift"]; 
-  let inputs = strings[Math.floor(Math.random() * strings.length)];
-
-  const gifs = await gf.search(inputs, { offset: offsetN });
-  aGif = "\""+ gifs.data[0].images.downsized.url+  "\"";
-  console.log(aGif);
-
-  giphy(aGif);
-
-};
-
-
 function giphy(aGif) {
-  var aGif = "https://media1.giphy.com/media/KZSU0FEIfsbis441IH/giphy-downsized.gif?cid=9f0f6425zabtehdzk94fw36gdhe4j8nrjxdc0kfpdf3diuzq&rid=giphy-downsized.gif";
+  
+  const randomGif = async () => {
+
+    const getRandomOffset = (max) => {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+    let offsetN = getRandomOffset(500);
+  
+    let strings = ["cute animals", "kittens", "puppies", "bts", "taylor swift"]; 
+    let inputs = strings[Math.floor(Math.random() * strings.length)];
+  
+    const gifs = await gf.search(inputs, { offset: offsetN });
+    aGif = gifs.data[0].images.downsized.url;
+    console.log(String(aGif));
+
+    giphy(aGif);
+  
+  };
 
   console.log(aGif);
 
@@ -40,10 +35,7 @@ function giphy(aGif) {
     <div className="App">
       <button onClick={randomGif}>click me!</button>
       <div className="gify">
-        <img 
-            src={aGif}
-            alt="a gif"
-        />
+        <a href={aGif}>link</a>
       </div>
     </div>
   );
